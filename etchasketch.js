519071afc284ad26d6ee6e62c6b2f7996945c8ef
Benjamin J. Navarro <benjnavarro28@gmail.com>
2024-06-03 17:25:33 -0500

    Cursor hover now draws on Etch-a-Sketch

--- etchasketch.js
+++ etchasketch.js
@@ -1,7 +1,21 @@
 const mainContainer = document.getElementById('container');
+let rows = 16;
+let columns = 16;
+mainContainer.style.width = '600px';
+mainContainer.style.height = '600px';
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    square.style.cssText = "border: 1px solid black; height: 24px; width: 24px";
-    mainContainer.appendChild(square);
+function changeSquareColor() {
+    this.style.backgroundColor = 'black';
 }
+
+function makeGrid() {
+    for (let i = 0; i < rows * columns; i++) {
+        const square = document.createElement('div');
+        square.style.cssText = "border: 1px solid black; height: 24px; width: 24px";
+        square.classList.add('grid-square');
+        mainContainer.appendChild(square);
+        square.addEventListener("mouseover", changeSquareColor);
+    }
+}
+
+makeGrid();
